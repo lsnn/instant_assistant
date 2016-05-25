fe334c1dbf7c57f5a08183ceafed33160a97cd09
Ivana Setiawan <ivana@funda.nl>
2016-05-25 17:06:04 +0200

    Fix error

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525102049) do
+ActiveRecord::Schema.define(version: 20160525130827) do
 
   create_table "assistants", force: :cascade do |t|
-    t.string   "full_name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
     t.string   "phone"
     t.datetime "created_at", null: false
@@ -27,10 +28,10 @@ ActiveRecord::Schema.define(version: 20160525102049) do
     t.string   "phone"
     t.string   "title"
     t.text     "description"
-    t.boolean  "available",    default: true
-    t.integer  "assistant_id"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.boolean  "available",    default: true
+    t.integer  "assistant_id"
   end
 
   create_table "users", force: :cascade do |t|

d8fd0fd5a39bc533a48ff0931ba0788e13d38b27
Ivana Setiawan <ivana@funda.nl>
2016-05-25 15:55:49 +0200

    Expired

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525102049) do
+ActiveRecord::Schema.define(version: 20160525130827) do
 
   create_table "assistants", force: :cascade do |t|
     t.string   "first_name"
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(version: 20160525102049) do
     t.string   "phone"
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "available",    default: true
+    t.integer  "assistant_id"
   end
 
   create_table "users", force: :cascade do |t|

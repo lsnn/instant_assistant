a84dcb872ea601f0bcab4bd8852106d76c83cc3b
Lisanne Fellinger <lisanne.88@gmail.com>
2016-05-25 22:39:32 +0200

    Adding button to request Uber

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'tasks/assign_task', to: 'tasks#assign_task'
   get 'tasks/cancel_task', to: 'tasks#cancel_task'
   get 'tasks/choose_location', to: 'tasks#choose_location'
+  get 'tasks/request_uber', to: 'tasks#request_uber'
 
   resources :tasks
   resources :assistants

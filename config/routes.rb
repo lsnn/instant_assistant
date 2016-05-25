cae9678926869e6cb66bf121ff5181a7b0ed06a3
Lisanne Fellinger <lisanne.88@gmail.com>
2016-05-25 22:24:05 +0200

    Add button and page to select location for uber

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'tasks/assign_task', to: 'tasks#assign_task'
   get 'tasks/cancel_task', to: 'tasks#cancel_task'
+  get 'tasks/choose_location', to: 'tasks#choose_location'
 
   resources :tasks
   resources :assistants

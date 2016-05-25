ef734ca572da10592c1010ae171fa922c108adcd
Lisanne Fellinger <lisanne.88@gmail.com>
2016-05-25 19:57:51 +0200

    Adding ability to cancel picked up task

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'tasks#new'
 
   get 'tasks/assign_task', to: 'tasks#assign_task'
+  get 'tasks/cancel_task', to: 'tasks#cancel_task'
 
   resources :tasks
   resources :assistants

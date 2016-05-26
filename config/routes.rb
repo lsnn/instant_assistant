6a1ddf065c242b23d2af9e9e985d7aedff312fbc
Lisanne Fellinger <lisanne.88@gmail.com>
2016-05-26 09:41:48 +0200

    Add confirm uber route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'tasks/cancel_task', to: 'tasks#cancel_task'
   get 'tasks/choose_location', to: 'tasks#choose_location'
   get 'tasks/request_uber', to: 'tasks#request_uber'
+  get 'tasks/confirm_uber', to: 'tasks#confirm_uber'
 
   resources :tasks
   resources :assistants

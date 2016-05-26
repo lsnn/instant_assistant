2eeedfd63299dfbdb5fff40837ad82ea833b5852
Lisanne Fellinger <lisanne.88@gmail.com>
2016-05-26 11:26:13 +0200

    Add page to follow car on

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'tasks/choose_location', to: 'tasks#choose_location'
   get 'tasks/request_uber', to: 'tasks#request_uber'
   get 'tasks/confirm_uber', to: 'tasks#confirm_uber'
+  get 'tasks/show_ride', to: 'tasks#show_ride'
 
   resources :tasks
   resources :assistants
